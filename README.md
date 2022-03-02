2d7a56e8c94d90b90ccdd40f7b9b332c3698366f
William Venner <william@venner.io>
2022-03-02 22:31:17 +0000

    release

---
+++ README.md
@@ -0,0 +1,28 @@
+[![crates.io](https://img.shields.io/crates/v/cpreprocess.svg)](https://crates.io/crates/cpreprocess)
+[![docs.rs](https://docs.rs/cpreprocess/badge.svg)](https://docs.rs/cpreprocess/)
+[![license](https://img.shields.io/crates/l/cpreprocess)](https://github.com/WilliamVenner/cpreprocess/blob/master/LICENSE)
+
+# cpreprocess
+
+Stupid and cursed Rust procedural macro that runs a C preprocessor on the input
+
+# Usage
+
+```toml
+[dependencies]
+cpreprocess = "*"
+```
+
+# Example
+
+```rust
+fn main() {
+	cpreprocess::cpreprocess! {"
+		#define MACRO(NAME) fn print_ ## NAME () { println!(\"hello world\"); }
+
+		MACRO(hello_world)
+
+		print_hello_world()
+	"}
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,39 @@
+//! Stupid and cursed Rust procedural macro that runs a C preprocessor on the input
+//!
+//! # Example
+//!
+//! ```no_run
+//! fn main() {
+//! 	cpreprocess::cpreprocess! {"
+//! 		#define MACRO(NAME) fn print_ ## NAME () { println!(\"hello world\"); }
+//!
+//! 		MACRO(hello_world)
+//!
+//! 		print_hello_world()
+//! 	"}
+//! }
+//! ```
+
 mod cpp;
 
 use proc_macro::TokenStream;
 
 #[proc_macro]
+/// Stupid and cursed Rust procedural macro that runs a C preprocessor on the input
+///
+/// # Example
+///
+/// ```no_run
+/// fn main() {
+/// 	cpreprocess::cpreprocess! {"
+/// 		#define MACRO(NAME) fn print_ ## NAME () { println!(\"hello world\"); }
+///
+/// 		MACRO(hello_world)
+///
+/// 		print_hello_world()
+/// 	"}
+/// }
+/// ```
 pub fn cpreprocess(tokens: TokenStream) -> TokenStream {
 	let tokens = syn::parse_macro_input!(tokens as syn::LitStr).value();
 	match cpp::preprocess(tokens.as_bytes())
